18ebdfe1d2879f818f5d4f716847a3e9875a1edd
Emil Skaug <abbeem12@gmail.com>
2026-04-20 20:56:42 +0200

    Update rp_handler.py

--- rp_handler.py
+++ rp_handler.py
@@ -3,10 +3,26 @@ import torch
 import runpod
 import time
 from transformers import pipeline
+from diffusers import StableDiffusionXLPipeline
+from huggingface_hub import login
 import os
+import base64
+from io import BytesIO
 
 def handler(event):
-    print("ENV DEBUG:", os.getenv("MY_KEY"))
+    key = os.getenv("MY_KEY")
+    print("ENV DEBUG:", key)
+    login(token=key)
+
+    # modell
+    model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+    
+    # ladda pipeline
+    pipe = StableDiffusionXLPipeline.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16,
+        use_safetensors=True
+    )
 
     print(f"Worker Start")
     input = event['input']
@@ -17,10 +33,20 @@ def handler(event):
     print(f"Received prompt: {prompt}")
     print(f"Sleeping for {seconds} seconds...")
 
-    # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
     time.sleep(seconds)  
 
-    return prompt 
+    # ====== ENDA TILLAGDA DELEN ======
+    print("Generating image...")
+    image = pipe(prompt).images[0]
+
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+    image_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    return {
+        "image_base64": image_base64
+    }
+
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler})
